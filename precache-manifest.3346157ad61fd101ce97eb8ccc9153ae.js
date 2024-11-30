98111c7c1ebff13eff432f0be52a80eb1649182e
Ishutachka <124629241+Ishuachka@users.noreply.github.com>
2024-11-30 16:38:45 +0300

    Add files via upload

---
+++ precache-manifest.3346157ad61fd101ce97eb8ccc9153ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abecaaf66120b994bf1a33260733eae3",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "dea1bc2567a21b21a3d5",
+    "url": "/dictionary/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/dictionary/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "dea1bc2567a21b21a3d5",
+    "url": "/dictionary/static/js/main.a7d62e0a.chunk.js"
+  },
+  {
+    "revision": "67d3f92be30e1c42b517",
+    "url": "/dictionary/static/js/runtime~main.dbf1fe99.js"
+  }
+]);
